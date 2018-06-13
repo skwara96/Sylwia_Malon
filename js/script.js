46e3dc261afa1f655b26d6442a6103b88c15d6ad
Adam Skwarczyński <skwara96@gmail.com>
2018-06-14 01:04:40 +0200

    gallery

--- js/script.js
+++ js/script.js
@@ -3,5 +3,8 @@ $(document).ready(function(){
         $(this).toggleClass('open');
     });
     
-    
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+                event.preventDefault();
+                $(this).ekkoLightbox();
+            });
 });
