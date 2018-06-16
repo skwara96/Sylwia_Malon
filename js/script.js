86e19874b6f3a1e48919bcdca34760e92fb1ed95
Adam Skwarczyński <skwara96@gmail.com>
2018-06-17 01:17:10 +0200

    animation

--- js/script.js
+++ js/script.js
@@ -11,4 +11,12 @@ $(document).ready(function () {
     $('.navbar-nav>li>a').on('click', function () {
         $('.navbar-collapse').collapse('hide');
     });
+    wow = new WOW({
+        boxClass: 'wow', // default
+        animateClass: 'animated', // default
+        offset: 0, // default
+        mobile: true, // default
+        live: true // default
+    })
+    new WOW().init();
 });
